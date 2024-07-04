1d9f411086d45027b244d87bbfbf28b1a3a7cf22
tsuyoshioyama <tsuyoshi.oyama@fox-hound.jp>
2024-07-04 16:53:29 +0900

    Create dashboard.stories.tsx

---
+++ app/dashboard/dashboard.stories.tsx
@@ -0,0 +1,51 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import Component from './dashboard';
+
+const meta = {
+  title: 'dashboard/Dashboard',
+  component: Component,
+  args: {
+    chart: {
+      data: [
+        { time: '00:00', amount: 0 },
+        { time: '03:00', amount: 200 },
+        { time: '06:00', amount: 600 },
+        { time: '09:00', amount: 800 },
+        { time: '12:00', amount: 1500 },
+        { time: '15:00', amount: 2000 },
+        { time: '18:00', amount: 2400 },
+        { time: '21:00', amount: 2400 },
+        { time: '24:00', amount: 2500 },
+      ],
+    },
+    deposit: {
+      data: { amount: 3024.0, date: '15 March, 2019' },
+    },
+    order: {
+      data: [
+        {
+          id: 1,
+          date: '16 Mar, 2019',
+          name: 'Elvis Presley',
+          shipTo: 'Tupelo, MS',
+          paymentMethod: 'VISA ⠀•••• 3719',
+          amount: 312.44,
+        },
+      ],
+    },
+  },
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-[1500px]">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Component>;
+export default meta;
+
+export const Dashboard: StoryObj<typeof meta> = {};

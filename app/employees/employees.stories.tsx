f93ddd7027c3a214a504f692d6e5b3b2d2d48ae1
tsuyoshioyama <tsuyoshi.oyama@fox-hound.jp>
2024-07-09 17:29:26 +0900

    figma画面の作成 社員画面の作成完了

--- app/employees/employees.stories.tsx
+++ app/employees/employees.stories.tsx
@@ -5,10 +5,134 @@ import Employees from './employees';
 const meta = {
   title: 'employees/employees',
   component: Employees,
+  args: {
+    data: [
+      {
+        id: 1,
+        icon: {
+          value: null,
+          backgroundColor: undefined,
+        },
+        name: 'サンプル',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 2,
+        icon: {
+          value: '北',
+          backgroundColor: 'bg-red-300',
+        },
+        name: '北島蓮也',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 3,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 4,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 5,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 6,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 7,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 8,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 9,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 10,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 11,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+      {
+        id: 12,
+        icon: {
+          value: '八',
+          backgroundColor: 'bg-blue-200',
+        },
+        name: '八尋祐輝',
+        email: 'test@example.com',
+        phoneNumber: '080-1234-1234',
+      },
+    ],
+  },
   parameters: {
-    layout: 'fullscreen',
+    layout: 'centered',
   },
 } satisfies Meta<typeof Employees>;
 export default meta;
 
-export const Main: StoryObj<typeof meta> = {};
+export const Employee: StoryObj<typeof meta> = {};
--- app/employees/employees.tsx
+++ app/employees/employees.tsx
@@ -1,40 +1,102 @@
-import { Box, Typography } from '@mui/material';
+'use client';
 
-export default function Employees() {
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Button, Paper, Tab, Tabs, Typography } from '@mui/material';
+import * as React from 'react';
+
+import { EmployeesData } from './types';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <Box className="bg-red-500 pl-3 pr-3 pt-3">
-      <Box className="bg-red-300 pb-10 pt-5">
-        <Typography component="h3" variant="h5" className="text-center font-bold text-indigo-500">
-          社員・協力会社
-        </Typography>
-        <Box className="flex">
-          <div className="text-black">社員一覧</div>
-          <div>協力会社一覧</div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Employees({ data }: EmployeesData) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <Box className="pb-10 pt-5">
+      <Typography component="h3" variant="h5" className="text-center font-bold text-indigo-500">
+        社員・協力会社
+      </Typography>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="社員一覧" {...a11yProps(0)} />
+            <Tab label="協力会社一覧" {...a11yProps(1)} />
+          </Tabs>
         </Box>
-        <Box>
-          <div className="text-right">社員を登録ボタン</div>
-          <table className="w-full table-fixed border-collapse">
-            <thead>
-              <tr className="table-header">
-                <th className="text-left">社員名</th>
-                <th className="text-center">メールアドレス</th>
-                <th className="text-center">電話番号</th>
-                <th className="text-right">編集ボタン</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="table-body">
-                <td className="flex">
-                  <div>アイコン</div>
-                  <div>名前</div>
-                </td>
-                <td className="text-center">test@example.com</td>
-                <td className="text-center">080-1234-1234</td>
-                <td className="text-right">編集ボタン</td>
-              </tr>
-            </tbody>
-          </table>
-        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <Paper className="px-10 pb-10 pt-5">
+            <Box className="mb-5 text-right">
+              <Button variant="contained" color="primary" size="medium" endIcon={<AddIcon />} href="#">
+                社員を登録
+              </Button>
+            </Box>
+            <table className="w-full table-fixed border-collapse">
+              <thead>
+                <tr className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-400 px-2 text-blue-600">
+                  <th className="py-2 text-left">社員名</th>
+                  <th className="py-2 text-left">メールアドレス</th>
+                  <th className="py-2 text-left">電話番号</th>
+                  <th className="w-16 text-center"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((employee) => (
+                  <tr
+                    key={employee.id}
+                    className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-400 p-2"
+                  >
+                    <td className="flex px-5 py-3">
+                      <div className={'w-[24px] rounded-full text-center' + ' ' + `${employee.icon.backgroundColor}`}>
+                        {employee.icon.value}
+                      </div>
+                      <div className="pl-5">{employee.name}</div>
+                    </td>
+                    <td className="text-left">{employee.email}</td>
+                    <td className="text-left">{employee.phoneNumber}</td>
+                    <td className="text-right">
+                      <Button variant="outlined" href="#">
+                        編集
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Paper>
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          Tab2
+        </CustomTabPanel>
       </Box>
     </Box>
   );

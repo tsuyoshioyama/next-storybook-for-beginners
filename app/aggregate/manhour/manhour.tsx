4d11f54512d749b9d971390b24c2ac1e0ef6fb1c
tsuyoshioyama <tsuyoshi.oyama@fox-hound.jp>
2024-07-12 13:26:17 +0900

    レイアウト修正のためにタグの追加、TextFieldタグ内にsize属性の追加
    ・画面全体単位でのレイアウトを整えられるようにdivタグを一つ追加しました。
    ・Paperタグとプルダウンメニューをひとまとめにできるように、これら二つの要素の親要素になるdivタグを追加してレイアウトを整えました。結果として37行目~87行目のコードの中身はほぼ修正していないのですが、全体的に修正を加えたような形になっています。
    ・TextFieldのサイズ指定が違っていたのでsize属性の追加しました。

--- app/aggregate/manhour/manhour.tsx
+++ app/aggregate/manhour/manhour.tsx
@@ -25,55 +25,68 @@ export default function Manhour({ mh }: { mh: ManhourData }) {
     { label: '12月', value: '12' },
   ];
   return (
-    <div>
+    <div className="px-3 pt-3">
       <div className="pb-10 pt-5">
-        <Typography component="h3" variant="h5" color="primary" className="mb-6 text-center font-bold">
-          工数集計
-        </Typography>
+        <div className="my-6">
+          <Typography component="h3" variant="h5" color="primary" className="mb-6 text-center font-bold">
+            工数集計
+          </Typography>
+        </div>
+        <div className="grid gap-3">
+          <div className="grid-cols-3 items-center">
+            <TextField
+              select
+              size="small"
+              variant="outlined"
+              className="mr-2 w-1/6"
+              defaultValue={currentYear.toString()}
+            >
+              {selectYear.map((year) => (
+                <MenuItem key={year.value} value={year.value}>
+                  {year.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField select size="small" variant="outlined" className="mr-2 w-1/6" defaultValue="1">
+              {selectMonth.map((month) => (
+                <MenuItem key={month.value} value={month.value}>
+                  {month.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button variant="outlined" size="medium" className="px-4 py-1.5" href="#">
+              表示
+            </Button>
+          </div>
+          <Paper className="px-10 pb-10 pt-5">
+            <Table>
+              <thead>
+                <tr className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-200 px-2 text-left text-blue-600">
+                  <th className="w-1/2 p-2">現場名</th>
+                  <th className="w-1/7 p-2">現場開始日時</th>
+                  <th className="w-1/7 p-2">現場最終日時</th>
+                  <th className="w-1/7 p-2">対象月の工数</th>
+                  <th className="w-1/7 p-2">総工数</th>
+                </tr>
+              </thead>
+              <tbody>
+                {mh.data.map((mhd) => (
+                  <tr
+                    key={mhd.name}
+                    className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-200 px-2"
+                  >
+                    <td className="px-2 py-4">{mhd.name}</td>
+                    <td className="px-2 py-4">{mhd.startAt}</td>
+                    <td className="px-2 py-4">{mhd.endAt}</td>
+                    <td className="px-2 py-4">{mhd.manHour}</td>
+                    <td className="px-2 py-4">{mhd.totalManHour}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Paper>
+        </div>
       </div>
-      <div className="flex">
-        <TextField select variant="outlined" className="mx-2 w-1/6" defaultValue="2024">
-          {selectYear.map((year) => (
-            <MenuItem key={year.value} value={year.value}>
-              {year.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField select variant="outlined" className="mx-2 w-1/6" defaultValue="1">
-          {selectMonth.map((month) => (
-            <MenuItem key={month.value} value={month.value}>
-              {month.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Button variant="outlined" size="small" className="mx-2 px-4 py-1.5" href="#">
-          表示
-        </Button>
-      </div>
-      <Paper className="px-10 pb-10 pt-5">
-        <Table>
-          <thead>
-            <tr className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-200 px-2 text-left text-blue-600">
-              <th className="w-1/2 p-2">現場名</th>
-              <th className="w-1/7 p-2">現場開始日時</th>
-              <th className="w-1/7 p-2">現場最終日時</th>
-              <th className="w-1/7 p-2">対象月の工数</th>
-              <th className="w-1/7 p-2">総工数</th>
-            </tr>
-          </thead>
-          <tbody>
-            {mh.data.map((mhd) => (
-              <tr key={mhd.name} className="border-x-0 border-b-[1px] border-t-0 border-solid border-slate-200 px-2">
-                <td className="px-2 py-4">{mhd.name}</td>
-                <td className="px-2 py-4">{mhd.startAt}</td>
-                <td className="px-2 py-4">{mhd.endAt}</td>
-                <td className="px-2 py-4">{mhd.manHour}</td>
-                <td className="px-2 py-4">{mhd.totalManHour}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Paper>
     </div>
   );
 }
